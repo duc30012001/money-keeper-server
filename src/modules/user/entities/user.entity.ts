e2d03100155612d1e67219e5cb11e8d8d09c01a7
duc30012001 <nguyenvietduc3001@gmail.com>
2025-04-26 00:42:03 +0700

    chore(user): add select password default

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -7,7 +7,7 @@ export class User extends BaseEntity {
 	@Column({ type: 'varchar', length: 255, unique: true })
 	email: string;
 
-	@Column({ type: 'varchar', length: 255, select: false })
+	@Column({ type: 'varchar', length: 255 })
 	@Exclude() // hides it from responses
 	password: string;
 
