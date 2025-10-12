e04a76f2cce8eed666b52be7a07a24c6c2af7e82
duc30012001 <nguyenvietduc3001@gmail.com>
2025-10-12 19:25:11 +0700

    update ssl config db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -16,9 +16,9 @@ export class DatabaseConfigService implements TypeOrmOptionsFactory {
 			password: this.configService.get<string>('DB_PASSWORD'),
 			database: this.configService.get<string>('DB_DATABASE'),
 
-			ssl: {
-				rejectUnauthorized: false, // if you don’t have a CA bundle
-			},
+			// ssl: {
+			// 	rejectUnauthorized: false, // if you don’t have a CA bundle
+			// },
 
 			// Entities
 			entities: [__dirname + '/../**/*.entity{.ts,.js}'],
